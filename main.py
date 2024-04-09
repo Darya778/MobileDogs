f833c463641c8fdf4be90f4a878afd371d23d13a
Darysha <112837727+Darya778@users.noreply.github.com>
2024-04-09 11:31:08 +0800

    Update main.py (регистрация user)

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+from fastapi import FastAPI, HTTPException
 
+app = FastAPI()
+
+# there should be a connection to the BD
+# ...
+
+# POST request для регистрации пользователей
+@app.post("/users/register")
+def register_user(login: str, password: str, email: str, gender: str, phone: str, birthday: str):
+  
+    # ПОДКЛЮЧЕНИЕ К БД !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    cnx = mysql.connector.connect(user=bdUser, password=bdPasswd,
+                                   host='localhost',
+                                  database=bdName)
+    cursor = cnx.cursor()
+  
+    # Проверяем, есть ли такой логин уже в базе
+    cursor.execute(f"SELECT 1 FROM users WHERE login = {login}")
+    if cursor.fetchone():
+        conn.close()
+        raise Exception("404: This login is already registered")
+
+    # Если логин уникален, добавляем его в базу
+    cursor.execute(f"INSERT INTO users (login, password, email, gender, phone, birthday) VALUES ({login}, {password}, {email}, {gender}, {phone}, {birthday})")
+    conn.commit()
+    conn.close()
+
+  
+    #users_db[id_user] = {
+    #    "login": login,
+    #    "password": password,
+    #    "email": email,
+    #    "gender": gender,
+    #    "phone": phone,
+    #    "birthday": birthday
+    #}
+    return {"message": "User registered successfully"}
