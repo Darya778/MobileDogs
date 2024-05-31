d08175b50f30034aa62fadf76f61edb929392945
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-31 13:20:34 +0800

    Update models.py

--- src/users/models.py
+++ src/users/models.py
@@ -1,1 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean, Date, TIMESTAMP
+from src.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    gender = Column(String(10))
+    phone = Column(String(20))
+    birthday = Column(Date)
+    registration_date = Column(TIMESTAMP, default='now()')
+    deletion_date = Column(TIMESTAMP, nullable=True)
+    archived = Column(Boolean, default=False)
 
