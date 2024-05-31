10200a44f1914d86a139eb804a6840e70a0ab1dc
Darysha <112837727+Darya778@users.noreply.github.com>
2024-05-31 19:42:15 +0800

    Add log files
    Файлы для логирования через ELK

---
+++ src/log/log_tasks.py
@@ -0,0 +1,26 @@
+import logging
+import ecs_logging
+import time
+from random import randint
+
+# Инициализация логгера
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('../log/log_tasks.json')
+handler.setFormatter(ecs_logging.StdlibFormatter())
+logger.addHandler(handler)
+
+def log_message(method, message):
+    """
+    Функция для вызова метода логгера.
+
+    Args:
+        method: Метод логгера (info, warning, error, critical).
+        message: Сообщение для записи в лог.
+    """
+
+    # Получение атрибута метода логгера по имени
+    log_method = getattr(logger, method)
+
+    # Вызов метода логгера
+    log_method(message, extra={"http.request.body.content": message})
