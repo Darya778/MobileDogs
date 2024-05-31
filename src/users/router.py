2c87cd3d7a272c523460b7c0f44fcb3325f4dc98
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-31 19:43:31 +0800

    Update router.py

--- src/users/router.py
+++ src/users/router.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(1, '../log')
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Tuple
@@ -7,6 +9,7 @@ from database_manager import  DatabaseManager
 from .schemas import *
 from sqlalchemy.exc import IntegrityError
 from users.hash_psw import get_password_hash
+from log_users import log_message
 
 router = APIRouter()
 db_manager=DatabaseManager('postgresql+psycopg2://user1:useruser@rc1b-2pdgtp1xwhfy0i47.mdb.yandexcloud.net:6432/mob_dogs?sslmode=verify-full')
@@ -19,25 +22,34 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         user_data["password"] = hashed_password
         user_id, user_answer = db_manager.add_user(db, **user_data)
         if user_id==0:
+            log_message("error", "500 "+str(user_answer))
             raise HTTPException(status_code=500,detail=user_answer)
+        log_message("info", "200 OK CREATED  - "+str(user_answer))
         return user_id, user_answer
     except Exception as e:
+        log_message("error", "500 "+str(e)+str(user_answer))
         raise HTTPException(status_code=500, detail=str(e)+str(user_answer))
 
 @router.put("/users/{user_id}", response_model=str)
 def update_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_db)):
     try:
         db_manager.update_user(db, user_id, **user_update.dict(exclude_unset=True))
+        log_message("info", "200 OK - Пользователь успешно обновлен.")
         return "Пользователь успешно обновлен."
     except Exception as e:
+        log_message("error", "500 "+str(e))
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/users/{user_id}", response_model=str)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     try:
         db_manager.archive_user(db, user_id)
+        log_message("info", "200 OK - Пользователь успешно удален.")
         return "Пользователь успешно удален."
     except Exception as e:
+        log_message("error", "500 "+str(e))
         raise HTTPException(status_code=500, detail=str(e))
+
+
 
 
