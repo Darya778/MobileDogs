54d1eaedf30516ab719375d34d22f5c7b0deb2e0
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-31 22:57:15 +0800

    Update add_user.py

--- scenaries/add_user.py
+++ scenaries/add_user.py
@@ -1,1 +1,18 @@
+import requests
+import json
+
+url = "http://localhost:8000/users/users"
+data = {
+    "login": "newuser",
+    "password": "password123",
+    "email": "newuser@example.com",
+    "gender": "male",
+    "phone": "1234567890",
+    "birthday": "2000-01-01T00:00:00",
+    "registration_date": "2023-01-01T00:00:00",
+    "deletion_date": None
+}
+
+response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(data))
+print(response.status_code, response.json())
 
