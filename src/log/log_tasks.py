28ce96389ffe0b5f8a0bff438a1469f523a61c81
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-31 22:42:22 +0800

    Update log_tasks.py

--- src/log/log_tasks.py
+++ src/log/log_tasks.py
@@ -6,7 +6,7 @@ from random import randint
 # Инициализация логгера
 logger = logging.getLogger("app")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('../log/log_tasks.json')
+handler = logging.FileHandler('log/log_tasks.json')
 handler.setFormatter(ecs_logging.StdlibFormatter())
 logger.addHandler(handler)
 
