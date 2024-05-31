733dba04eb625568fe8ecea623b1c1fb505cb02c
Posolot <138215725+Posolot@users.noreply.github.com>
2024-05-31 17:56:46 +0800

    Update models.py

--- src/devices/models.py
+++ src/devices/models.py
@@ -31,15 +31,4 @@ class Coordinate(Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     timestamp = Column(TIMESTAMP, default='now()')
-
-class Task(Base):
-    __tablename__ = 'tasks'
-
-    id = Column(Integer, primary_key=True)
-    id_user_1 = Column(Integer, ForeignKey('users.id'), nullable=False)
-    id_user_2 = Column(Integer, ForeignKey('users.id'), nullable=False)
-    id_task = Column(Integer, nullable=False)
-    confirm = Column(Boolean, default=False)
-    send_date = Column(TIMESTAMP, default='now()')
-    completion_date = Column(TIMESTAMP, nullable=True)
-    archived = Column(Boolean, default=False)
+    
