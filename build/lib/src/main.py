2ab49fc8f6eea36532c0890a12e605448daca1ab
Darysha <112837727+Darya778@users.noreply.github.com>
2024-06-01 08:21:26 +0800

    Add files via upload

---
+++ build/lib/src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from users.router import router as user_router
+from devices.router import router as device_router
+from tasks.router import router as tasks_router
+
+app = FastAPI()
+
+app.include_router(user_router, prefix="/users", tags=["users"])
+app.include_router(device_router, prefix="/device", tags=["devices"])
+app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the IoT Project API"}
